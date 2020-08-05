956bd2df1a10c2d71cf7d4bafd610054daeab03f
Dmitry Kalashnikov <dmkalash.mail@gmail.com>
2020-08-05 17:05:57 +0300

    db error fixed

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 
 from bot import bot
 from threading import Timer
-from config import database
 import config
 import logging
 import handlers
@@ -11,8 +10,8 @@ if __name__ == "__main__":
     logger = logging.getLogger('vmquest')
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
     logger.setLevel(logging.DEBUG)
-    # database = db_init()
-    database.connect()
+    #config.database = config.db_init()
+    config.database.connect()
 
     from timer import check_time
     if config.MODE == config.OFFLINE:
