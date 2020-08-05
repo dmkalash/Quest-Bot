71e0b15243808eeacbb690c0abb73d880afb1ba3
Dmitry Kalashnikov <dmkalash.mail@gmail.com>
2020-08-05 15:40:03 +0300

    logging

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from bot import bot
 from threading import Timer
 from timer import check_time
 import config
-#import logging
+import logging
 import handlers
 
 if __name__ == "__main__":
-    #logger = logging.getLogger('vmquest')
-    #logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-    #logger.setLevel(logging.DEBUG)
+    logger = logging.getLogger('vmquest')
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+    logger.setLevel(logging.DEBUG)
     if config.MODE == config.OFFLINE:
         t = Timer(config.CHECKER_TIME, check_time)
         t.start()
