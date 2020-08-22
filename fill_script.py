fa63a99faa9e0d8f62e1df542717eba4d31b962b
Dmitry Kalashnikov <dmkalash.mail@gmail.com>
2020-08-22 19:36:57 +0300

    Fill-Fabric created

--- fill_script.py
+++ fill_script.py
@@ -1,10 +1,22 @@
 # -*- coding: utf-8 -*-
 from utils import create_tables, fill_on_points, fill_off_points, fill_on_reactions, fill_off_reactions, fill_files
 
+class FillFactory:
+    def __init__(self):
+        self._fillers = {
+            'create_tables': create_tables,
+            'on_points': fill_on_points,
+            'off_points': fill_off_points,
+            'on_reactions': fill_on_reactions,
+            'off_reactions': fill_off_reactions,
+            'files': fill_files
+        }
+
+    def run(self):
+        for key in self._fillers:
+            self._fillers[key]()
+            print(key, ': OK')
+
 def fill_script():
-    print('create_tables', create_tables())
-    print('on_points:', fill_on_points())
-    print('off_points:', fill_off_points())
-    print('on_reactions:', fill_on_reactions())
-    print('off_reactions:', fill_off_reactions())
-    print('fill_files:', fill_files())
+    factory = FillFactory()
+    factory.run()
