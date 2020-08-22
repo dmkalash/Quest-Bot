9b652483313da05d15a98433e0ea0fc084c4a9d8
Dmitry Kalashnikov <dmkalash.mail@gmail.com>
2020-08-22 20:02:52 +0300

    fill functionality is placed in own file

---
+++ fill.py
@@ -0,0 +1,25 @@
+import view
+import pandas as pd
+from exception_guard import exception_guard
+from models import Team, OnPoint, OffPoint, OnReaction, OffReaction, File
+from msg import messages
+from config import *
+from config import database
+
+class FillFactory:
+    def __init__(self):
+        self._fillers = {
+            'on_points': view.point.add_on_point,
+            'off_points': view.point.add_off_point,
+            'reactions': view.reaction.add_reaction, # TODO: СОЗДАТЬ КОЛОНКУ С ТИПОМ В CSV: ONLINE/OFFLINE!!!!!
+            'files': view.file.add_file
+        }
+
+    @exception_guard
+    def fill(self, format, file_path):
+        data = pd.read_csv(file_path)
+        for index, row in data.iterrows():
+            kwargs = {key: row[key] for key in row.keys()}
+            self._fillers[format](kwargs) # TODO: когда буду вводить эту функцию, поменять add-функцию на kwargs
+            print(index, 'OK')
+        return SUCCESS  # TODO: выводить OK и SUCCESS только если правда все успешно
