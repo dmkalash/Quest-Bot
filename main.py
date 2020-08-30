9327703484304b92022ade5e44b5f54dbd17172e
Dmitry Kalashnikov <dmkalash.mail@gmail.com>
2020-08-30 21:58:13 +0300

    visual fixes

--- main.py
+++ main.py
@@ -3,17 +3,16 @@
 from db_init import database
 from bot import bot
 from threading import Timer
-#import db_init
 import config
 import logging
 import util_handlers
 import user_handlers
 
-# TODO: db_init в отдельный файл
 # TODO: сделать все команды удобнее
 # TODO: сделать листинг команд для разраба отдельной командой
 # TODO: выделить все константные ответы в messages
 # TODO: решить, делать ли тесты
+# TODO: нормальные отступы
 
 if __name__ == "__main__":
     logger = logging.getLogger('vmquest')
